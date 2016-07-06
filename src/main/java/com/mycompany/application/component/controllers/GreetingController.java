9841841f7de0dfe2c4d6d03e697f8d2555036419
arvindashtekar <arvind.ashtekar@gmail.com>
2016-07-06 15:55:31 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/application/component/controllers/GreetingController.java
@@ -0,0 +1,41 @@
+package com.mycompany.application.component.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.application.component.dto.Greeting;
+
+@RestController
+public class GreetingController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = null;
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails)principal).getUsername();
+		} else {
+			username = principal.toString();
+		}
+		logger.debug("Logged in user name is: "+ username );
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
